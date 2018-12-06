63112ad853db01b31783c4c647a514232da73769
artem <artem@mmtr.ru>
2018-12-06 16:05:39 +0300

    Title changed

--- views/login.html.php
+++ views/login.html.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="./favicon.ico">
 
-    <title>Product example for Bootstrap</title>
+    <title>MMTR office map</title>
 
     <!-- Bootstrap core CSS -->
     <link href="./dist/css/bootstrap.min.css" rel="stylesheet">
